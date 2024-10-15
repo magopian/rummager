95fb18d7f35153b53f9ea6c5ec40d6ebc389e503
Mathieu Agopian <mathieu@agopian.info>
2024-10-16 00:44:26 +0200

    Initial commit

---
+++ start_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+@onready var start_button: Button = %StartButton
+@onready var quit_button: Button = %QuitButton
+
+
+func _ready() -> void:
+	start_button.pressed.connect(_on_start_button_pressed)
+	quit_button.pressed.connect(_on_quit_button_pressed)
+	start_button.grab_focus()
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
