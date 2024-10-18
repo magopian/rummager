ec9638f8880bdde134329ee510876027cdc9e001
Mathieu Agopian <mathieu@agopian.info>
2024-10-18 23:43:47 +0200

    Display the version number in the start screen

--- start_menu.gd
+++ start_menu.gd
@@ -3,12 +3,15 @@ extends Control
 
 @onready var start_button: Button = %StartButton
 @onready var quit_button: Button = %QuitButton
+@onready var version_label: Label = %VersionLabel
 
 
 func _ready() -> void:
 	start_button.pressed.connect(_on_start_button_pressed)
 	quit_button.pressed.connect(_on_quit_button_pressed)
 	start_button.grab_focus()
+	version_label.text += ProjectSettings.get_setting("application/config/version")
+
 
 
 func _on_start_button_pressed() -> void:
