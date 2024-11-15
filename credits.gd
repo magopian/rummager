45ed80c03c8fdf25df44809c8f66c203d0dd6da5
Mathieu Agopian <mathieu@agopian.info>
2024-11-15 12:27:48 +0100

    Slight refactor of the credits and add transition back to menu

--- credits.gd
+++ credits.gd
@@ -1,14 +1,15 @@
-extends Control
+extends CanvasLayer
 
 
 @onready var menu_button: Button = %MenuButton
-@onready var click_to_change_scene: CanvasLayer = %ClickToChangeScene
+@onready var click_catch: TextureButton = %ClickCatch
+@onready var fade_transition: ColorRect = %FadeTransition
 
 
 func _ready() -> void:
 	menu_button.pressed.connect(_on_menu_pressed)
-	click_to_change_scene.button_pressed.connect(_on_menu_pressed)
+	click_catch.pressed.connect(_on_menu_pressed)
 
 
 func _on_menu_pressed() -> void:
-	get_tree().change_scene_to_file("res://start_menu.tscn")
+	fade_transition.fade_to_file("res://start_menu.tscn")
