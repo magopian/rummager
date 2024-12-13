52ea0802dc607b8753bcf3349b73d0b171101ca0
Mathieu Agopian <mathieu@agopian.info>
2024-12-13 11:42:19 +0100

    Small bits and general cleanup

--- animated_button.gd
+++ animated_button.gd
@@ -17,7 +17,7 @@ func _ready() -> void:
 	button.text = text
 	button.add_theme_constant_override("outline_size", outline_size)
 	button.add_theme_constant_override("font_size",  font_size)
-	button.pivot_offset = Vector2(button.size.x / 2, button.size.y / 2)
+	button.pivot_offset = button.size / 2
 	
 	var tween: Tween = button.create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC).set_loops()
 	tween.tween_property(button, "scale", Vector2(1.1, 1.1), 0.5)
