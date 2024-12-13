59fb6a42d2e3c01e70af4df502f02bd7ee61471b
Mathieu Agopian <mathieu@agopian.info>
2024-12-13 09:22:34 +0100

    Animate the palette button to draw some attention to it

--- palette_button.gd
+++ palette_button.gd
@@ -4,15 +4,36 @@ extends Control
 @onready var card_1: TextureButton = %Card1
 @onready var card_2: TextureButton = %Card2
 
+
+var tween: Tween
+
+
 func _ready() -> void:
 	Global.user_prefs.apply_preferences()
 	card_1.pressed.connect(_on_card_pressed)
 	card_2.pressed.connect(_on_card_pressed)
-	card_1.change_palette_to(Global.get_next_palette())
-	card_2.change_palette_to(Global.get_next_palette())
+	pivot_offset = size / 2
+	animate_palette_switching()
+
+
+func animate_palette_switching() -> void:
+	if tween and tween.is_running():
+		tween.stop()
+	tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_interval(2)
+	tween.tween_callback(change_palette_to.bind(Global.get_next_palette()))
+	tween.parallel().tween_property(self, "rotation_degrees", 5, 0.2)
+	tween.tween_property(self, "rotation_degrees", -5, 0.4)
+	tween.tween_property(self, "rotation_degrees", 0, 0.2)
+	tween.tween_callback(change_palette_to.bind(Global.palette))
+	tween.set_loops()
 
 
 func _on_card_pressed() -> void:
 	Global.change_palette()
-	card_1.call_deferred("change_palette_to", Global.get_next_palette())
-	card_2.call_deferred("change_palette_to", Global.get_next_palette())
+	animate_palette_switching()
+
+
+func change_palette_to(palette: int) -> void:
+	card_1.change_palette_to(palette)
+	card_2.change_palette_to(palette)
