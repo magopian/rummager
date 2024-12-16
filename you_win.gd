8f979fbc6cc084fb9f4dc5e9d82b2af586a0aad2
Mathieu Agopian <mathieu@agopian.info>
2024-12-16 09:32:05 +0100

    Ask if you want the next level (10 more cards) or replay the current one

--- you_win.gd
+++ you_win.gd
@@ -3,19 +3,26 @@ extends CanvasLayer
 @onready var catch_all_click: TextureButton = %CatchAllClick
 @onready var fade_transition: ColorRect = %FadeTransition
 @onready var next_level_button: AnimatedButton = %NextLevelButton
+@onready var replay_button: Button = %ReplayButton
 
 
 func _ready() -> void:
 	fade_transition.show()
-	catch_all_click.pressed.connect(_on_button_pressed)
-	next_level_button.button_pressed.connect(_on_button_pressed)
+	catch_all_click.pressed.connect(_on_next_level_pressed)
+	next_level_button.button_pressed.connect(_on_next_level_pressed)
+	replay_button.pressed.connect(_on_replay_pressed)
 	fade_transition.fade_in()
 	print("time elapsed memorizing: ", Global.time_started_rummage - Global.time_started)
 	print("time elapsed rummaging: ", Time.get_ticks_msec() - Global.time_started_rummage)
 	print("number of cards discarded: ", Global.discarded_cards)
 
 
-func _on_button_pressed() -> void:
+func _on_next_level_pressed() -> void:
 	Global.level += 1
+	Music.play_theme()
+	fade_transition.fade_to_file("res://game.tscn")
+
+
+func _on_replay_pressed() -> void:
 	Music.play_theme()
 	fade_transition.fade_to_file("res://game.tscn")
