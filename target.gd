cf90f7bfc1fa0659a4281f6585d04315fd764df2
Mathieu Agopian <mathieu@agopian.info>
2024-12-20 10:49:23 +0100

    Better target animation

--- target.gd
+++ target.gd
@@ -17,10 +17,12 @@ func _ready() -> void:
 
 
 func setup_curve():
-	var bottom_left: Vector2 = Vector2(0, Global.viewport_size.y - 100)
+	var height: int = Global.viewport_size.y - 100
+	var width: int = Global.viewport_size.x
+	var bottom_left: Vector2 = Vector2(0, height)
 	var top_left: Vector2 = Vector2.ZERO
-	var top_right: Vector2 = Vector2(Global.viewport_size.x, 0)
-	var bottom_right: Vector2 = Vector2(Global.viewport_size.x, Global.viewport_size.y - 100)
+	var top_right: Vector2 = Vector2(width, 0)
+	var bottom_right: Vector2 = Vector2(width, height)
 	path_2d.curve = Curve2D.new()
 	path_2d.curve.add_point(bottom_left, Vector2.ZERO, Vector2.ZERO)
 	path_2d.curve.add_point(top_left, Vector2.ZERO, Vector2.ZERO)
@@ -31,10 +33,19 @@ func setup_curve():
 func random_target() -> void:
 	var random_progress_ratio: float = randf()
 	
+	var tween_duration: float = compute_tween_duration(path_follow_2d.progress_ratio, random_progress_ratio)
 	var tween: Tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
-	tween.tween_property(path_follow_2d, "progress_ratio", random_progress_ratio, 1)
-	tween.tween_property(target, "modulate:a", 0.5, 1)
+	tween.tween_property(path_follow_2d, "progress_ratio", random_progress_ratio, tween_duration)
+	tween.parallel().tween_property(target, "modulate:a", 0.5, tween_duration)
 	await tween.finished
+
+
+func compute_tween_duration(ratio1: float, ratio2: float) -> float:
+	"""Take two ratios, and return their difference.
+
+	This works because the max difference is 1, which is the longest duration we want.
+	"""
+	return max(ratio1, ratio2) - min(ratio1, ratio2)
 
 
 func _on_target_hit(body: Node2D) -> void:
