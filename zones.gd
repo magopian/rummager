fed3d542107a2a713bef7f0b4597e7c7c8861358
Mathieu Agopian <mathieu@agopian.info>
2025-01-29 12:37:00 +0100

    Rework the zones: no more lerping when getting closer, no more red, green zone always visible, pulsating

--- zones.gd
+++ zones.gd
@@ -1,29 +1,15 @@
 extends CanvasLayer
 
 
-@onready var zone_top: TextureRect = %ZoneTop
-@onready var zone_left: TextureRect = %ZoneLeft
-@onready var zone_right: TextureRect = %ZoneRight
 @onready var zone_bottom: TextureRect = %ZoneBottom
 
 
-var viewport_rect: Vector2
-
-
 func _ready() -> void:
-	viewport_rect = get_viewport().get_visible_rect().size
-
-
-func _process(_delta: float) -> void:
-	var mouse_pos: Vector2 = get_viewport().get_mouse_position()
-	var distance_to_left: float = clamp(mouse_pos.x, 0, viewport_rect.x / 2) / (viewport_rect.x / 2)
-	var distance_to_top: float = clamp(mouse_pos.y, 0, viewport_rect.y / 2) / (viewport_rect.y / 2)
-	var distance_to_right: float = clamp(viewport_rect.x - mouse_pos.x, 0, viewport_rect.x / 2)/ (viewport_rect.x / 2)
-	var distance_to_bottom: float = clamp(viewport_rect.y - mouse_pos.y, 0, viewport_rect.y / 2)/ (viewport_rect.y / 2)
-	zone_left.modulate.a = lerp(1, 0, distance_to_left)
-	zone_top.modulate.a = lerp(1, 0, distance_to_top)
-	zone_right.modulate.a = lerp(1, 0, distance_to_right)
-	zone_bottom.modulate.a = lerp(1, 0, distance_to_bottom)
+	var tween: Tween = zone_bottom.create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD)
+	tween.set_loops(0)
+	tween.tween_property(zone_bottom, "modulate:a", 0.3, 0.5)
+	tween.tween_property(zone_bottom, "modulate:a", 1.0, 0.2)
+	tween.tween_interval(1)
 
 
 func slide_in(duration: float) -> void:
